0e3b4620853bab28ef3774b887d8427a778c72c5
bhlindemann <bh@lindemannrock.com>
2026-04-05 13:50:41 +0200

    docs(translations): add English translations for Component Manager plugin

---
+++ src/translations/en/component-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Component Manager plugin for Craft CMS 5.x
+ *
+ * @link      https://lindemannrock.com
+ * @copyright Copyright (c) 2026 LindemannRock
+ */
+
+return [
+    'Component Manager' => 'Component Manager',
+    'Organize components, validate props, and document your design system from one control panel workspace.' => 'Organize components, validate props, and document your design system from one control panel workspace.',
+    'Open Component Manager' => 'Open Component Manager',
+];
